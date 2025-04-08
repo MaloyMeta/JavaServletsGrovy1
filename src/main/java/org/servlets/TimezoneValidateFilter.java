b42cc6fd3d840ffe5788ab67b378c4f6db9d9235
MaloyMeta <supermaks2211@gmail.com>
2025-04-08 23:17:44 +0300

    TimezoneValidateFilter *update*

--- src/main/java/org/servlets/TimezoneValidateFilter.java
+++ src/main/java/org/servlets/TimezoneValidateFilter.java
@@ -20,21 +20,21 @@ public class TimezoneValidateFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
             throws IOException, ServletException {
         String notFormatedUTC = req.getParameter("timezone");
+        String formattedUTC;
+        try {
+            if (notFormatedUTC != null) {
+                formattedUTC = parseUTC(notFormatedUTC); //Форматуємо для правильного формату offset
+            }
+            else {
+                formattedUTC = "+00:00";
+            }
+            req.setAttribute("formattedUTC", formattedUTC); //передаємо на сервлет відформатований атрибут
+            chain.doFilter(req, resp); // пропускаємо
 
-        if (notFormatedUTC != null){
-            try{
-                String formattedUTC = parseUTC(notFormatedUTC); //Форматуємо для правильного формату offset
-                req.setAttribute("formattedUTC", formattedUTC); //передаємо на сервлет відформатований атрибут
-                chain.doFilter(req,resp); // пропускаємо
-
-        } catch(IllegalArgumentException e){
+        }catch(IllegalArgumentException e){
             HttpServletResponse httpResponse = (HttpServletResponse) resp;
             httpResponse.sendError(HttpServletResponse.SC_BAD_REQUEST, "Невірний UTC формат: " + e.getMessage());
         }
-    }else{
-            HttpServletResponse httpResponse = (HttpServletResponse) resp;
-            httpResponse.sendError(HttpServletResponse.SC_BAD_REQUEST, "Обов'язковий параметр UTC!");
-         }
     }
 
 }
